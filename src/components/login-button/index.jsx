c7e0d2fc31dcaf80b9d492dfe0f1ebeb82ae8922
Joel Coutinho <joelcoutinho@qually.io>
2021-03-06 11:04:26 +0530

    server setup

---
+++ src/components/login-button/index.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from 'react-router-dom';
+
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <Link to='/dashboard'>Login</Link>
+        )
+    }
+
+}
+export default App;
